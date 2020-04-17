b3fa4d8820fb9a59ae4899a09cb96d7825ae9eef
Andrew <aschult5@gmail.com>
2020-04-17 11:48:48 -0700

    Appearance refresh: white primary

--- scripts/toggle-redshift.sh
+++ scripts/toggle-redshift.sh
@@ -2,8 +2,7 @@
 
 if pgrep -x redshift > /dev/null
 then
-	pgrep redshift | xargs -n1 kill -9 | notify-send 'Redshift stopped'
+	killall -q -w redshift
 else
-	notify-send 'Redshift started'
 	redshift &
 fi
